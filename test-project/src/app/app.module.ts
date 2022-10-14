c833478b1d959171041834995d153c472f00814e
1996-isuru <isurusrimalnt@gmail.com>
2022-10-14 09:30:16 +0530

    ss

--- test-project/src/app/app.module.ts
+++ test-project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TtComponent } from './tt/tt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TtComponent
   ],
   imports: [
     BrowserModule,
